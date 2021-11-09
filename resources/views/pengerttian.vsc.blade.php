51f36b60a9adff5c03b84bcf6b045ceac0691688
LAPTOP-52TL75LM\ASUS <60900119019@uin-alauddin.ac.id>
2021-11-09 12:06:49 +0700

    pengertianVCS

--- resources/views/pengerttian.vsc.blade.php
+++
@@ -1,2 +0,0 @@
-<!-- pengertian visual studio code -->
-<!-- Visual studio Code merupakan aplikasi cross platform yang dapat digunakan berbagai sitem operasi seperti windows, Linux, dan Mac OS. -->
---
+++ resources/views/pengerttian_vsc.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Visual studio Code merupakan aplikasi cross platform yang dapat digunakan berbagai sitem operasi seperti windows, Linux, dan Mac OS.</p>
+</body>
+</html>

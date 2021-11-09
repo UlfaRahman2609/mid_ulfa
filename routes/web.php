e6b2d1fbd29f3682cd0029e3d3ab063741bac4ef
LAPTOP-52TL75LM\ASUS <60900119019@uin-alauddin.ac.id>
2021-11-09 12:00:17 +0700

    cakke, 26 september 1999

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Tempat Tanggal Lahir : Cakke ');
+    return view('Tempat Tanggal Lahir : Cakke 26 september 1999 ');
 });
